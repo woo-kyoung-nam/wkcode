d65254732d738b2fc135db6b5172a74b47384478
woo-kyoung-nam <112924560+woo-kyoung-nam@users.noreply.github.com>
2025-08-27 00:31:56 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 0.54 ms, Memory: 9.38 MB -BaekjoonHub

---
+++ 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.py
@@ -0,0 +1,8 @@
+def solution(numbers):
+    answer = []
+    numbers = sorted(numbers)
+    
+    for i in range(len(numbers)):
+        for j in range(i+1, len(numbers)):
+            answer.append(numbers[i] + numbers[j])
+    return sorted(set(answer))
